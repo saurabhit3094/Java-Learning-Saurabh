581c0ed55cd009d2de89ff09429ed1dbc904e7bd
saurabh <saurabhit3094@gmail.com>
2018-02-01 13:04:32 +0530

    "Initial Commit"

---
+++ MultiThreading/Multithreading-Task1/src/main/java/org/Multithreading/Task1/App.java
@@ -0,0 +1,13 @@
+package org.Multithreading.Task1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
